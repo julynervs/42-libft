c852b2effed8b6b3192957a5bd6dcf49af15a542
Julyelly Roberta <julyellyr@gmail.com>
2021-09-24 13:32:06 -0300

    Added corrections to ft_wordfill function

--- ft_split.c
+++ ft_split.c
@@ -5,73 +5,69 @@
 /*                                                    +:+ +:+         +:+     */
 /*   By: jurobert <jurobert@student.42sp.org.br>    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
-/*   Created: 2021/09/03 23:32:37 by jurobert          #+#    #+#             */
-/*   Updated: 2021/09/09 13:17:40 by jurobert         ###   ########.fr       */
+/*   Created: 2021/09/24 12:56:41 by jurobert          #+#    #+#             */
+/*   Updated: 2021/09/24 13:27:00 by jurobert         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "libft.h"
 
-static void	ft_fillwords(size_t n_words, char **str, char c);
-static size_t	ft_countwords(char const *s, char c);
+static size_t	ft_wordcount(char const *s, char c)
+{
+	size_t	count;
+	size_t	i;
+
+	count = 0;
+	i = 0;
+	while (s[i] != '\0')
+	{
+		if (s[i] != c)
+		{
+			while (s[i] != c && s[i] != '\0')
+				i++;
+			count++;
+		}
+		else
+			i++;
+	}
+	return (count);
+}
+
+static char	**ft_wordfill(char const *s, char c, char **tab)
+{
+	size_t	i;
+	size_t	start_char;
+	size_t	n;
+
+	i = 0;
+	n = 0;
+	while (s[i] != '\0')
+	{
+		if (s[i] != c)
+		{
+			start_char = i;
+			while (s[i] != c && s[i] != '\0')
+				i++;
+			tab[n] = ft_substr(s, start_char, (i - start_char));
+			n++;
+		}
+		else
+			i++;
+	}
+	return (tab);
+}
 
 char	**ft_split(char const *s, char c)
 {
-	char	**str;
+	char	**tab;
+	size_t	n_words;
 
 	if (!s)
 		return (0);
-	str = calloc(ft_countwords(s, c) + 1, sizeof(char *));
-	if (!str)
+	n_words = ft_wordcount(s, c);
+	tab = ft_calloc(n_words + 1, sizeof(char *));
+	if (!tab)
 		return (0);
-	ft_fillwords(ft_countwords(s, c) + 1, str, c);
-	return (str);
-}
-
-static size_t	ft_countwords(char const *s, char c)
-{
-	size_t	i;
-	size_t	n_words;
-
-	i = 0;
-	n_words = 0;
-	while (s[i] != '\0')
-	{
-		if (s[i] == c)
-			i++;
-		else
-		{
-			while (s[i] != c && s[i] != '\0')
-			{
-				i++;
-			}
-			n_words++;
-		}
-	}
-	return (n_words);
-}
-
-static void	ft_fillwords(size_t n_words, char **str, char c)
-{
-	size_t	n;
-	char	*start;
-	size_t	len_word;
-
-	n = 0;
-	start = (char *)str;
-	while (n < n_words)
-	{
-		if (*start == c && *start != '\0')
-			start++;
-		else
-		{
-			len_word = 0;
-			while (*start != c && *start != '\0')
-			{
-				start++;
-				len_word++;
-			}
-		}
-		str[n++] = ft_substr(start, 0, len_word);
-	}
+	tab = ft_wordfill(s, c, tab);
+	return (tab);
 }
