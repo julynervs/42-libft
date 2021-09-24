8c9076ed69889a724b6388587737b2dd36b8c18f
Julyelly Roberta <julyellyr@gmail.com>
2021-09-24 14:02:51 -0300

    Changed pointer arithmetic to array index

--- ft_strncmp.c
+++ ft_strncmp.c
@@ -14,18 +14,16 @@
 
 int	ft_strncmp(const char *s1, const char *s2, size_t len)
 {
+	size_t			i;
 	unsigned char	*str1;
 	unsigned char	*str2;
 
 	str1 = (unsigned char *)s1;
 	str2 = (unsigned char *)s2;
+	i = 0;
 	if (len == 0)
 		return (0);
-	while (len && str1[len] && (*str1 == *str2))
-	{
-		str1++;
-		str2++;
-		len--;
-	}
-	return (*str1 - *str2);
+	while (str1[i] && (str1[i] == str2[i]) && (i < len - 1))
+		i++;
+	return (str1[i] - str2[i]);
 }
