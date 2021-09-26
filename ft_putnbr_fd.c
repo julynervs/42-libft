9273ed33542017545b27e6c1690045eccedbf723
Julyelly Roberta <julyellyr@gmail.com>
2021-09-26 20:43:50 -0300

    Changed function scope

--- ft_putnbr_fd.c
+++ ft_putnbr_fd.c
@@ -14,5 +14,15 @@
 
 void	ft_putnbr_fd(int n, int fd)
 {
-	ft_putstr_fd(ft_itoa(n), fd);
+	char	*str;
+	unsigned int	num;
+
+	if (n < 0)
+	{
+		str[0] = '-';
+		num = -n;
+	}
+	while (num >= 10)
+		ft_putnbr_fd(num % 10, fd);
+	ft_putnbr_fd(num / 10, fd);
 }
