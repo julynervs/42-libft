31c3c6074f8817d5deb65e763ead98662860b9e1
Julyelly Roberta <julyellyr@gmail.com>
2021-09-26 20:51:52 -0300

    Changed while loop to if conditional for recursively using

--- ft_putnbr_fd.c
+++ ft_putnbr_fd.c
@@ -22,7 +22,7 @@ void	ft_putnbr_fd(int n, int fd)
 		ft_putchar_fd('-', fd);
 		num = -n;
 	}
-	while (num >= 10)
+	if (num >= 10)
 		ft_putnbr_fd(num / 10, fd);
 	ft_putchar_fd((num % 10) + '0', fd);
 }
