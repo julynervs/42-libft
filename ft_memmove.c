ec538af6b14def4ce21cf375dc3552e7721b1a4d
Julyelly Roberta <julyellyr@gmail.com>
2021-09-29 19:02:23 -0300

    Changed return

--- ft_memmove.c
+++ ft_memmove.c
@@ -6,7 +6,7 @@
 /*   By: jurobert <jurobert@student.42sp.org.br>    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2021/08/27 09:48:00 by jurobert          #+#    #+#             */
-/*   Updated: 2021/08/30 14:29:34 by jurobert         ###   ########.fr       */
+/*   Updated: 2021/09/29 18:59:49 by jurobert         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -23,9 +23,8 @@ void	*ft_memmove(void *dst, const void *src, size_t len)
 	{
 		while (len--)
 			*(d + len) = *(s + len);
-		return ((void *)dst);
 	}
 	else
 		ft_memcpy(dst, src, len);
-	return (0);
+	return ((void *)dst);
 }
