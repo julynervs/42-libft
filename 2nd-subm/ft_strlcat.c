f65288cc14a438be170e2d97ee2b71988051ef44
Julyelly Roberta <julyellyr@gmail.com>
2021-10-05 17:27:14 -0300

    Changed variable names

--- 2nd-subm/ft_strlcat.c
+++ 2nd-subm/ft_strlcat.c
@@ -6,28 +6,32 @@
 /*   By: jurobert <jurobert@student.42sp.org.br>    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2021/08/28 19:19:01 by jurobert          #+#    #+#             */
-/*   Updated: 2021/10/05 12:45:00 by jurobert         ###   ########.fr       */
+/*   Updated: 2021/10/05 17:26:32 by jurobert         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "libft.h"
 
-size_t	ft_strlcat(char *dst, const char *src, size_t size)
+size_t	ft_strlcat(char *dst, const char *src, size_t newsize)
 {
 	size_t	i;
 	size_t	j;
+	size_t	dst_len;
+	size_t	src_len;
 
-	if (size < ft_strlen(dst) + 1)
-		return (size + ft_strlen(src));
+	dst_len = ft_strlen(dst);
+	src_len = ft_strlen(src);
+	if (newsize < dst_len + 1)
+		return (newsize + src_len);
 	i = 0;
 	while (dst[i] != '\0')
 		i++;
 	j = 0;
-	while ((i + j < size - 1) && src[j])
+	while ((i + j < newsize - 1) && src[j])
 	{
 		dst[i + j] = src[j];
 		j++;
 	}
 	dst[i + j] = '\0';
-	return (i + ft_strlen(src));
+	return (dst_len + src_len);
 }
