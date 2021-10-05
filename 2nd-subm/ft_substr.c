1314fd33bfbfc2c135f6232eba7024ff9b54b138
Julyelly Roberta <julyellyr@gmail.com>
2021-10-05 12:18:08 -0300

    Added ft_substr fixes

--- 2nd-subm/ft_substr.c
+++ 2nd-subm/ft_substr.c
@@ -6,7 +6,7 @@
 /*   By: jurobert <jurobert@student.42sp.org.br>    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2021/09/03 17:33:19 by jurobert          #+#    #+#             */
-/*   Updated: 2021/10/05 12:12:23 by jurobert         ###   ########.fr       */
+/*   Updated: 2021/10/05 12:17:38 by jurobert         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -17,17 +17,19 @@ char	*ft_substr(char const *str, unsigned int start, size_t len)
 	char	*substr;
 	size_t	str_len;
 
-	if (!str)
-		return (NULL);
-	str_len = ft_strlen(str);
-	if (str_len <= len)
-		len = str_len - start;
-	else if (start + len >= str_len)
-		return (ft_strdup(""));
-	substr = ft_calloc(len + 1, sizeof(char));
-	if (!substr)
-		return (NULL);
-	else
-		ft_strlcpy(substr, str + start, len + 1);
-	return (substr);
+	if (str)
+	{
+		str_len = ft_strlen(str);
+		if (start >= str_len)
+			return (ft_strdup(""));
+		else if (start + len >= str_len)
+			len = str_len - start;
+		substr = ft_calloc(len + 1, sizeof(char));
+		if (!substr)
+			return (NULL);
+		else
+			ft_strlcpy(substr, str + start, len + 1);
+		return (substr);
+	}
+	return (NULL);
 }
