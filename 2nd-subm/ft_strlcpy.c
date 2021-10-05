0dc9ad2280f2c6cf38da9250f38430145ae2780b
Julyelly Roberta <julyellyr@gmail.com>
2021-10-05 16:45:06 -0300

    Changed variables name for better understanding

--- 2nd-subm/ft_strlcpy.c
+++ 2nd-subm/ft_strlcpy.c
@@ -6,25 +6,25 @@
 /*   By: jurobert <jurobert@student.42sp.org.br>    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2021/08/30 13:24:54 by jurobert          #+#    #+#             */
-/*   Updated: 2021/10/05 12:25:09 by jurobert         ###   ########.fr       */
+/*   Updated: 2021/10/05 16:31:03 by jurobert         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "libft.h"
 
-size_t	ft_strlcpy(char *dst, const char *src, size_t size)
+size_t	ft_strlcpy(char *dst, const char *src, size_t dstsize)
 {
-	size_t	len;
+	size_t	src_len;
 
-	len = ft_strlen(src);
-	if (size <= 0)
-		return (len);
-	else if (len + 1 < size)
-		ft_memmove(dst, src, len + 1);
-	else if (len + 1 >= size)
+	src_len = ft_strlen(src);
+	if (dstsize <= 0)
+		return (src_len);
+	else if (dstsize > src_len + 1)
+		ft_memmove(dst, src, src_len + 1);
+	else if (dstsize <= src_len + 1)
 	{
-		ft_memmove(dst, src, size - 1);
-		dst[size - 1] = '\0';
+		ft_memmove(dst, src, dstsize - 1);
+		dst[dstsize - 1] = '\0';
 	}
-	return (len);
+	return (src_len);
 }
