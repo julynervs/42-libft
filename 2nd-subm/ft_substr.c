38f48707e1f630655c9193c1be4a78ad4706840c
Julyelly Roberta <julyellyr@gmail.com>
2021-10-05 12:22:20 -0300

    Added ft_substr variable fixes

--- 2nd-subm/ft_substr.c
+++ 2nd-subm/ft_substr.c
@@ -6,7 +6,7 @@
 /*   By: jurobert <jurobert@student.42sp.org.br>    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2021/09/03 17:33:19 by jurobert          #+#    #+#             */
-/*   Updated: 2021/10/05 12:20:14 by jurobert         ###   ########.fr       */
+/*   Updated: 2021/10/05 12:22:05 by jurobert         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -16,18 +16,20 @@ char	*ft_substr(char const *str, unsigned int start, size_t len)
 {
 	char	*substr;
 	size_t	str_len;
+	size_t	sub_len
 
 	if (str)
 	{
 		str_len = ft_strlen(str);
+		sub_len = len;
 		if (start >= str_len)
 			return (ft_strdup(""));
 		else if (start + len >= str_len)
-			len = str_len - start;
-		substr = ft_calloc(len + 1, sizeof(char));
+			sub_len = str_len - start;
+		substr = ft_calloc(sub_len + 1, sizeof(char));
 		if (!substr)
 			return (NULL);
-		ft_strlcpy(substr, (str + start), len + 1);
+		ft_strlcpy(substr, (str + start), sub_len + 1);
 		return (substr);
 	}
 	return (NULL);
