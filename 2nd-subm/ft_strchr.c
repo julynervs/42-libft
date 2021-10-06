feca4bf1b38070087ed09cd5477d678426171aa6
Julyelly Roberta <julyellyr@gmail.com>
2021-10-06 19:29:51 -0300

    Added norm fixes

--- 2nd-subm/ft_strchr.c
+++ 2nd-subm/ft_strchr.c
@@ -6,7 +6,7 @@
 /*   By: jurobert <jurobert@student.42sp.org.br>    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2021/08/30 17:53:13 by jurobert          #+#    #+#             */
-/*   Updated: 2021/10/05 11:36:33 by jurobert         ###   ########.fr       */
+/*   Updated: 2021/10/06 19:29:10 by jurobert         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -14,7 +14,9 @@
 
 char	*ft_strchr(const char *s, int c)
 {
-	unsigned char cc = (unsigned char)c;
+	unsigned char	cc;
+
+	cc = (unsigned char)c;
 	while (*s != '\0' && *s != cc)
 		s++;
 	if (*s == cc || cc == '\0')
